569a4d4bbe96ededcbfb4225a615f80855dbe618
DavidsonMuldrow <dm882824@ohio.edu>
2026-04-08 13:22:01 -0400

    Inputs and Stats

---
+++ My project/Assets/Scripts/PlayerStats.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+[CreateAssetMenu(menuName = "Player Movement")]
+public class PlayerStats : MonoBehaviour
+{
+    [Header("Move")]
+    [Range(1f, 100f)] public float MoveSpeed = 13f;
+    [Range(0.25f, 50f)] public float GroundAcceleration = 5f;
+    [Range(0.25f, 50f)] public float GroundDeceleration = 20f;
+    [Range(0.25f, 50f)] public float AirAcceleration = 5f;
+    [Range(0.25f, 50f)] public float AirDeceleration = 5f;
+
+    [Header("Grounded/Collision Checks")]
+    public LayerMask GroundLayer;
+    public float GroundDetectionRayLength = 0.02f;
+    public float HeadDetectionRayLength = 0.02f;
+    [Range(0f, 1f)] public float HeadWidth = 0.75f;
+
+}
